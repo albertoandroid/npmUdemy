64ca88e6cfb5b0b7096514c49420462b7ff98760
Alberto <info@androiddesdecero.com>
2019-08-17 21:10:45 +0200

    npm2

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 //var start = new Date(2021, 00, 01);
 //var end = new Date(2021, 00, 18);
 
+const d3 = require('d3-time')
 
 var start = new Date(2021, 02, 01);
 var end = new Date(2021, 03, 01);
@@ -9,6 +10,10 @@ var end = new Date(2021, 03, 01);
 var miliSegundosDia = 24*60*60*1000;
 
 var resultado = (end-start)/miliSegundosDia;
+
+console.log(resultado);
+
+resultado = d3.timeDay.count(start, end);
 
 console.log(resultado);
 
