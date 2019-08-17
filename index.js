eb63700a103d80d60bf26a21286c798d54d137e1
Alberto <info@androiddesdecero.com>
2019-08-17 20:41:56 +0200

    npm

---
+++ index.js
@@ -0,0 +1,14 @@
+
+//var start = new Date(2021, 00, 01);
+//var end = new Date(2021, 00, 18);
+
+
+var start = new Date(2021, 02, 01);
+var end = new Date(2021, 03, 01);
+
+var miliSegundosDia = 24*60*60*1000;
+
+var resultado = (end-start)/miliSegundosDia;
+
+console.log(resultado);
+
